297b988b049581c4d49e0a384e23628da6e408f2
Miklós Koren <miklos.koren@gmail.com>
2018-02-27 21:33:04 +0100

    valasztopolgarok, ervenyes es ervenytelen szavazatok szama szavazokoronkent

--- adat/jelolt/parse_precincts.py
+++ adat/jelolt/parse_precincts.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os, csv, re
-import lxml.html
+import lxml.html, lxml.etree
 import glob
 
 '''
@@ -10,6 +10,7 @@ Jelolt-id: 2014 / OEVK # / jelolt sorszam
 
 EGYENI_RE = re.compile(r'<p style="text-align:left;">A szavazatok száma jelöltenként</p>(.*?</table>)', re.UNICODE and re.DOTALL)
 LISTAS_RE = re.compile(r'<p style="text-align:left;">A szavazatok száma pártlistánként</p>(.*?</table>)', re.UNICODE and re.DOTALL)
+RESZVETEL_RE = re.compile(r'(<th>AE</th>.*?<th>NE</th>.*?</table>)', re.UNICODE and re.DOTALL)
 NONPRINTABLE_RE = re.compile(r'[\W_]+', re.UNICODE)
 
 
@@ -22,18 +23,21 @@ def clean_cell(text):
 def numeric_value(text):
     return int(NONPRINTABLE_RE.sub('', text))
 
-def html_table(etre_element):
+def html_table(etree_elements):
     output = []
-    rows = iter(etre_element)
-    headers = [clean_cell(col.text) for col in next(rows)]
-    for row in rows:
-        values = [clean_cell(col.text) for col in row]
-        output.append(dict(zip(headers, values)))
+    for element in etree_elements:
+        # last <th> element is used as header
+        headers = [clean_cell(col.text) for col in element.xpath('tr[th]')[-1]]
+        for row in element.xpath('tr[td]'):
+            values = [clean_cell(col.text) for col in row]
+            output.append(dict(zip(headers, values)))
     return output
 
-def find_table(html, regex):
-    matches = regex.search(html)
-    return lxml.html.fromstring(matches.group(1)).xpath('//table')[0]
+def find_tables(html, regex):
+    table_text = regex.search(html).group(1)
+    if table_text[0:4]=='<th>':
+        table_text = '<table><tr>'+table_text
+    return lxml.html.fromstring(table_text).xpath('//table')
 
 def oevk_mapping(filename):
     reader = csv.DictReader(open(filename, 'r'))
@@ -62,6 +66,19 @@ def store_listas(row, szervezet_lista, eredmeny_lista):
             pass 
     eredmeny_lista.append(dict(szavazokor=row['szavazokor'], part=jid, szavazat=numeric_value(row['Szavazat'])))
 
+def store_reszvetel(row, reszvetel_lista):
+    if 'EE' in row:
+        # kulkepviseletes
+        row['valasztopolgarok'] = numeric_value(row['EE'])
+    else:
+        row['valasztopolgarok'] = numeric_value(row['AE'])
+    row['ervenyes'] = numeric_value(row['NE'])
+    row['ervenytelen'] = numeric_value(row['ME'])
+    reszvetel_lista.append(dict(szavazokor=row['szavazokor'], 
+        valasztopolgarok=row['valasztopolgarok'],
+        ervenyes=row['ervenyes'],
+        ervenytelen=row['ervenytelen']))
+
 def parse_file(filename, datastore):
     html = open(filename,'r', encoding='latin2').read()
     m1 = re.search('M\d{2}/T\d{3}/', filename)
@@ -69,13 +86,19 @@ def parse_file(filename, datastore):
     szavazokor_id = m1.group(0)+m2.group(0)
 
     for fajl in fuggvenyek.keys():
-        output = html_table(find_table(html, fuggvenyek[fajl]))
-        for row in output:
+        output = html_table(find_tables(html, fuggvenyek[fajl]))
+        if fajl=='reszvetel':
+            row = {}
+            [row.update(table) for table in output]
             row['szavazokor'] = szavazokor_id
-            if fajl=='egyeni':
-                store_relation(row, datastore['jelolt'], datastore['egyeni'], datastore['oevk_map'])
-            else:
-                store_listas(row, datastore['szervezet'], datastore['listas'])
+            store_reszvetel(row, datastore['reszvetel'])
+        else:
+            for row in output:
+                row['szavazokor'] = szavazokor_id
+                if fajl=='egyeni':
+                    store_relation(row, datastore['jelolt'], datastore['egyeni'], datastore['oevk_map'])
+                elif fajl=='listas':
+                    store_listas(row, datastore['szervezet'], datastore['listas'])
 
 def write_csv(list_name, datastore):
     data = datastore[list_name]
@@ -84,16 +107,16 @@ def write_csv(list_name, datastore):
     for row in data:
         writer.writerow(row)
 
-fuggvenyek = {'egyeni': EGYENI_RE, 'listas': LISTAS_RE}
+fuggvenyek = {'egyeni': EGYENI_RE, 'listas': LISTAS_RE, 'reszvetel': RESZVETEL_RE}
 
 if __name__ == '__main__':
 
     oevk_map = oevk_mapping('../telepules/szavazokor_oevk_2014.csv')
-    datastore = dict(oevk_map=oevk_map, jelolt=[], egyeni=[], szervezet=[], listas=[])
+    datastore = dict(oevk_map=oevk_map, jelolt=[], egyeni=[], szervezet=[], listas=[], reszvetel=[])
     file_list = glob.glob('html/M??/T???/*.html')
 
     for filename in file_list:
         parse_file(filename, datastore)
 
-    for key in ['jelolt', 'egyeni', 'szervezet', 'listas']:
+    for key in ['jelolt', 'egyeni', 'szervezet', 'listas', 'reszvetel']:
         write_csv(key, datastore)
