a88758d474fbb540edeb33cab13c4b17408c44ed
Miklos Koren <miklos.koren@gmail.com>
2018-03-20 16:04:55 +0100

    google: megyei valasztasi erdeklodes

---
+++ adat/part/google_trends/convert_google.py
@@ -0,0 +1,53 @@
+import csv
+import re
+from datetime import datetime
+import glob
+
+FIELD_RE = re.compile(r'(.+?):\W\((.+?)\)', re.UNICODE and re.DOTALL)
+NONPRINTABLE_RE = re.compile(r'[\W_]+', re.UNICODE)
+
+def numeric_value(text):
+    return int(NONPRINTABLE_RE.sub('', text))
+
+def parse_field(field):
+	match = FIELD_RE.search(field)
+	if match:
+		return dict(search_term=match.group(1), geo=match.group(2))
+
+
+def convert_dictionary(row):
+	fields_to_reshape = [key for key in list(row.keys()) if parse_field(key) is not None]
+	output = [parse_field(key) for key in fields_to_reshape]
+	for key, value in row.items():
+		if key in fields_to_reshape:
+			i = fields_to_reshape.index(key)
+			# insert in proper row
+			output[i].update(dict(search_volume=numeric_value(value)))
+		else:
+			# common variables, insert in all rows
+			[line.update({key.lower(): value}) for line in output]
+	return output
+
+
+def read_csv(filename):
+	with open(filename, 'r') as f:
+		[f.readline() for _ in range(2)]
+		return list(csv.DictReader(f))
+
+def write_csv(list_name, datastore):
+    writer = csv.DictWriter(open('{}.csv'.format(list_name), 'w', encoding='utf-8'), fieldnames=datastore[0].keys())
+    writer.writeheader()
+    for row in datastore:
+        writer.writerow(row)
+
+if __name__ == '__main__':
+
+    datastore = []
+    file_list = glob.glob('reszvetel/*.csv')
+
+    for filename in file_list:
+        for line in read_csv(filename):
+        	datastore.extend(convert_dictionary(line))
+
+    write_csv('reszvetel', datastore)
+
