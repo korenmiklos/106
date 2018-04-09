411b8fef9575df43772b40ba90fa9ed68e32e216
Miklós Koren <miklos.koren@gmail.com>
2018-04-09 06:59:19 +0200

    2018-as jegyzokonyvek

---
+++ adat/2018/scrape.sh
@@ -0,0 +1,2 @@
+# wget --no-host-directories --directory-prefix=html --cut-dirs=4 -x -i szavazokorok_results.txt 
+cat szavazokorok_results.txt | parallel wget --no-host-directories --directory-prefix=html --cut-dirs=4 -x  
